b69e33be51fc7adc6a9644ba9af5acc6af5c756f
manish <manishpamnani169@gmail.com>
2020-11-15 11:52:37 +0530

    Added submit functinality

--- script.js
+++ script.js
@@ -4,6 +4,10 @@
 // D->Delete
 
 
+// fetch('http://localhost:3000/books')
+//     .then(response => response.json())
+//     .then(bookData => console.log(bookData))
+
 fetch('http://localhost:3000/books')
     .then(response => response.json())
     .then(bookData => console.log(bookData))
@@ -40,9 +44,45 @@ fetch('http://localhost:3000/books')
 // .catch(error => console.log(error))
 
 // DELETE
-fetch('http://localhost:3000/books/4', {
-    method: 'DELETE',
+// fetch('http://localhost:3000/books/4', {
+//     method: 'DELETE',
+//     headers: {
+//         'Content-Type' : 'application/json'
+//     }
+// }).then(response => response.json())
+
+function onSubmit() {
+    console.log(document.getElementById('title').value)
+    console.log(document.getElementById('author').value)
+    console.log(document.getElementById('coverImage').value)
+    console.log(document.getElementById('description').value)
+
+    fetch('http://localhost:3000/books', {
+    method: 'POST',
+    body: JSON.stringify({
+        title: document.getElementById('title').value,
+        author: document.getElementById('author').value,
+        coverImage: document.getElementById('coverImage').value,
+        description: document.getElementById('description').value
+    }),
     headers: {
         'Content-Type' : 'application/json'
     }
 }).then(response => response.json())
+.then(bookData => {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('coverImage').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('success').innerHTML = 'Success'
+    setTimeout(() => {
+        document.getElementById('success').hidden = true;
+    }, 2000);
+})
+.catch(error => {
+    document.getElementById('error').innerHTML = 'Error'
+    setTimeout(() => {
+        document.getElementById('error').hidden = 'Error'
+    }, 2000);
+})
+}
